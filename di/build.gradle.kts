37d7121bc11ba8a271b8fc37e897bab354f9ff43
iliyangermanov <iliyan.germanov971@gmail.com>
2024-12-14 22:40:40 +0200

    Fix deprecation

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -1,51 +1,55 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.publish)
+  alias(libs.plugins.kotlinMultiplatform)
+  alias(libs.plugins.androidLibrary)
+  alias(libs.plugins.publish)
 }
 
 kotlin {
-    jvm()
-    androidTarget {
-        publishLibraryVariants("release")
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-        }
+  jvm()
+  androidTarget {
+    publishLibraryVariants("release")
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+      jvmTarget.set(JvmTarget.JVM_1_8)
     }
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-    linuxX64()
-    js(IR) {
-        browser()
-        nodejs()
-    }
-    @OptIn(ExperimentalWasmDsl::class) wasmJs()
+  }
+  iosX64()
+  iosArm64()
+  iosSimulatorArm64()
+  linuxX64()
+  js(IR) {
+    browser()
+    nodejs()
+  }
+  @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
+  wasmJs {
+    browser()
+    nodejs()
+    d8()
+  }
 
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                //put your multiplatform dependencies here
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(libs.kotlin.test)
-                implementation(libs.bundles.test)
-            }
-        }
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        //put your multiplatform dependencies here
+      }
     }
+    val commonTest by getting {
+      dependencies {
+        implementation(libs.kotlin.test)
+        implementation(libs.bundles.test)
+      }
+    }
+  }
 }
 
 android {
-    namespace = "com.ivy_apps.di"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
+  namespace = "com.ivy_apps.di"
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
+  defaultConfig {
+    minSdk = libs.versions.android.minSdk.get().toInt()
+  }
 }
